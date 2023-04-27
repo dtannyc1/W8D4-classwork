da84f688fc1a6d96cf9e07babe9f39a576822154
dtannyc1 <dtannyc1@gmail.com>
2023-04-27 17:35:18 -0400

    made pipes move

--- practice-for-ch-js-flappy-bird-long-practice-main/src/level.js
+++ practice-for-ch-js-flappy-bird-long-practice-main/src/level.js
@@ -1,6 +1,9 @@
 export default class Level {
     constructor(dimensions) {
         this.dimensions = dimensions;
+        this.pipes = [({x:   0, y: Math.random()*(this.dimensions.height-CONSTANTS.GAP_SIZE)}),
+                      ({x: CONSTANTS.DX, y: Math.random()*(this.dimensions.height-CONSTANTS.GAP_SIZE)}),
+                      ({x: CONSTANTS.DX * 2, y: Math.random()*(this.dimensions.height-CONSTANTS.GAP_SIZE)})];
     }
 
     drawBackground(ctx) {
@@ -10,5 +13,33 @@ export default class Level {
 
     animate(ctx) {
         this.drawBackground(ctx);
+        this.movePipes.bind(this)();
+        this.drawPipes(ctx);
+    }
+
+    drawPipes(ctx) {
+        this.pipes.forEach((pipe) => {
+            ctx.fillStyle = "green";
+            ctx.fillRect(pipe.x, 0, CONSTANTS.PIPE_WIDTH, pipe.y);
+            ctx.fillRect(pipe.x, pipe.y + CONSTANTS.GAP_SIZE, CONSTANTS.PIPE_WIDTH, this.dimensions.height);
+        });
+    }
+
+    movePipes() {
+        this.pipes.forEach((pipe) => {
+            pipe.x += CONSTANTS.PIPE_SPEED;
+        });
+
+        if (this.pipes[0].x + CONSTANTS.PIPE_WIDTH < 0){
+            this.pipes.shift();
+            this.pipes.push(({x: this.pipes[this.pipes.length-1].x + CONSTANTS.DX, y: Math.random()*(this.dimensions.height-CONSTANTS.GAP_SIZE)}));
+        }
     }
 }
+
+const CONSTANTS = {
+    GAP_SIZE:  150,
+    DX:  220,
+    PIPE_WIDTH:  50,
+    PIPE_SPEED: -2
+  };
